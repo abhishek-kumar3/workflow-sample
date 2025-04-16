ba1e520f08d043cfeb344c34a481e6a6e3b242ea
Abhishek Kumar <abhi.go@hotmail.com>
2025-04-16 21:59:20 +0530

    initial commit

---
+++ src/main/java/dev/abhi/workflow/sample/WorkflowSampleApplication.java
@@ -0,0 +1,13 @@
+package dev.abhi.workflow.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowSampleApplication.class, args);
+	}
+
+}
